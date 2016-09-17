d4a9396d824c9e866bcfaeccf0a03fccbf0d6b46
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-17 23:30:28 +0800

    微調

--- models/access-token.js
+++ models/access-token.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const requestPromise = require('request-promise');
-const url=require('url');
+const url = require('url');
 
 var clientId = 'd852506dbe7e48fb8c769a7b7065176f';
 var redirectUri = 'https://ig-crawler.herokuapp.com/callback';
@@ -14,8 +14,9 @@ function requestCode(req, res) {
     })
 }
 
-function getCode(req, res){
+function getCode(req, res) {
     res.send(req.query.code);
 }
 
+exports.requestCode = requestCode;
 exports.getCode = getCode;
