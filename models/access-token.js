4f3e72ff83ca5727d7f780ff7e94c3d87b638b5c
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-17 23:14:54 +0800

    加入access-token模組接口

--- models/access-token.js
+++ models/access-token.js
@@ -2,7 +2,7 @@ const request = require('request');
 const requestPromise = require('request-promise');
 var clientId = 'd852506dbe7e48fb8c769a7b7065176f';
 var redirectUri = 'https://ig-crawler.herokuapp.com/callback';
-var code='';
+var code = '';
 
 function getCode(req, res) {
     requestPromise('https://instagram.com/oauth/authorize/?client_id=' + clientId + '&redirect_uri=' + redirectUri + '&response_type=code').then((body) => {
@@ -11,3 +11,5 @@ function getCode(req, res) {
         res.send(error);
     })
 }
+
+exports.getCode = getCode;
