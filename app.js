626da2eacc9535eaeeff59982d2326550f6de784
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-18 01:27:25 +0800

    微調

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const app = express();
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
-app.use(cookieParser({
-    cookieSecret: '@2uaiigol'
-}));
+app.use(cookieParser('@2uaiigol'));
 app.use(session());
 app.use(require('./controllers'));
 app.listen(port, () => { console.log('now listening on port ' + port) });
