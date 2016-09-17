0f1bbc6b4baf8ee81d24134cbae26676fc2c5097
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-17 23:28:22 +0800

    加入getCode function

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const accessToken=require('../models/access-token.js');
 
-router.get('/token', accessToken.getCode);
+router.get('/token', accessToken.requestCode);
+router.get('/callback', accessToken.getCode);
 module.exports = router;
--- models/access-token.js
+++ models/access-token.js
@@ -1,15 +1,21 @@
 const request = require('request');
 const requestPromise = require('request-promise');
+const url=require('url');
+
 var clientId = 'd852506dbe7e48fb8c769a7b7065176f';
 var redirectUri = 'https://ig-crawler.herokuapp.com/callback';
 var code = '';
 
-function getCode(req, res) {
+function requestCode(req, res) {
     requestPromise('https://instagram.com/oauth/authorize/?client_id=' + clientId + '&redirect_uri=' + redirectUri + '&response_type=code').then((body) => {
         res.send(body);
     }).catch((error) => {
         res.send(error);
     })
+}
+
+function getCode(req, res){
+    res.send(req.query.code);
 }
 
 exports.getCode = getCode;
