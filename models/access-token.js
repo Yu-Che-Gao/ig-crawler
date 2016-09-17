1438fbc1a691acb26b8c0c0b50b699ede1a59bba
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-17 23:39:47 +0800

    code清理

--- models/access-token.js
+++ models/access-token.js
@@ -1,10 +1,5 @@
-const request = require('request');
-const requestPromise = require('request-promise');
-const url = require('url');
-
 var clientId = 'd852506dbe7e48fb8c769a7b7065176f';
 var redirectUri = 'https://ig-crawler.herokuapp.com/callback';
-var code = '';
 
 function requestCode(req, res) {
     res.redirect('https://instagram.com/oauth/authorize/?client_id=' + clientId + '&redirect_uri=' + redirectUri + '&response_type=code');
