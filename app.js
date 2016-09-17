0efdd56f6f4e061087017fe1c8ec97b080c3ea16
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-18 01:34:06 +0800

    微調

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const app = express();
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
-app.use({
+app.use(session({
     secret: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', // 建议使用 128 个字符的随机字符串
     cookie: { maxAge: 86400 * 24 * 30 }
-});
-app.use(session());
+}));
 app.use(require('./controllers'));
 app.listen(port, () => { console.log('now listening on port ' + port) });
